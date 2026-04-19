ae8feab9f617a048c8f2c21fb9eb285dcf1ba97a
Tj-Bots <tjbots123@gmail.com>
2026-04-20 02:45:44 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+from os import environ 
+
+class Config:
+    API_ID = int(environ.get("API_ID", ""))
+    API_HASH = environ.get("API_HASH", "")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_SESSION = environ.get("BOT_SESSION", "tjbot") 
+    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
+    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+
+
+
+class temp(object): 
+    lock = {}
+    CANCEL = {}
+    forwardings = 0
+    BANNED_USERS = []
+    IS_FRWD_CHAT = []
+
